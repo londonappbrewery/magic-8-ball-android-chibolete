086bad208788760736aa3be4d5f4244f0c358413
Lucas Padula <lucas.padula@venturus.org.br>
2019-09-18 11:03:22 -0300

    Finish button logic

--- app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,11 +17,24 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button bt = findViewById(R.id.bt_ask);
+        final ImageView imgView = findViewById(R.id.iv_magicBall);
+
+        final int[] ballImgIds = {R.drawable.ball1,
+                R.drawable.ball2,
+                R.drawable.ball3,
+                R.drawable.ball4,
+                R.drawable.ball5
+        };
 
         bt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Log.d("aaa", "potato");
+
+                Random rand = new Random();
+                int imgIndex = rand.nextInt(5);
+
+                imgView.setImageResource(ballImgIds[imgIndex]);
             }
         });
     }
